dd9371095344a3e3eed09a5003891af6dad6f451
Michael Scott <michael.scott@autotrader.co.uk>
2016-11-29 13:58:35 +0000

    Turn Order
    Made the turns order sequential rather than random, to make it more usable for the initial usage.

--- src/main/java/com/mscott/timer/group/GroupList.java
+++ src/main/java/com/mscott/timer/group/GroupList.java
@@ -2,13 +2,12 @@ package com.mscott.timer.group;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.apache.commons.lang3.RandomUtils;
 
 public class GroupList {
 
     private ObservableList<String> groupNames = FXCollections.observableArrayList();
 
-    private int currentPersonIndex;
+    private int currentPersonIndex = -1; // initialise with -1, so we start with the first person in the list
 
     public ObservableList<String> getGroupNames() {
         return groupNames;
@@ -30,11 +29,12 @@ public class GroupList {
 
     public String getNextPerson() {
 
-        int newPersonIndex;
-        do {
-            newPersonIndex = RandomUtils.nextInt(0, groupNames.size());
-        } while (newPersonIndex != currentPersonIndex);
-        currentPersonIndex = newPersonIndex;
+        if (currentPersonIndex >= (groupNames.size() - 1)) {
+            // hit the end of the names, wrap to the beginning
+            currentPersonIndex = 0;
+        } else {
+            currentPersonIndex++;
+        }
 
         return groupNames.get(currentPersonIndex);
     }
